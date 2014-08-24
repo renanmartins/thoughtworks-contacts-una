f6164dd66a3390dcd9a30cbdbb4b926cdc9359fd
Renan Martins <renanmartins89@gmail.com>
2014-08-24 15:21:50 -0300

    Adds couple of protractor end to end tests

--- test/e2e/homePagesSpec.js
+++ test/e2e/homePagesSpec.js
@@ -1,10 +1,44 @@
-describe('Home Pages', function() {
+describe('ThoughtWorks Contacts', function() {
+  var groups;
 
-  var ptor = protractor.getInstance();
-
-  it('should load the homepage', function() {
-    ptor.get('#');
-    expect(true).toBe(true);
+  beforeEach(function () {
+    browser.get('#');
+    groups = element.all(by.repeater('(group,contacts) in groups'));
   });
 
+  it('should group existent contacts alphabetically', function() {
+    expect(groups.count()).toBe(4);
+    expect(getGroupName(0)).toEqual('A');
+    expect(getGroupName(1)).toEqual('P');
+    expect(getGroupName(2)).toEqual('R');
+    expect(getGroupName(3)).toEqual('T');
+  });
+
+  it('should contain correct contacts in each group', function() {
+    getGroupContacts(0).then(function(contacts) {
+      expect(contacts[0].getText()).toEqual('Anabela');
+    });
+    getGroupContacts(1).then(function(contacts) {
+      expect(contacts[0].getText()).toEqual('Paulo Brito');
+      expect(contacts[1].getText()).toEqual('Pedro Legal');
+    });
+    getGroupContacts(2).then(function(contacts) {
+      expect(contacts[0].getText()).toEqual('Renan Martins');
+    });
+    getGroupContacts(3).then(function(contacts) {
+      expect(contacts[0].getText()).toEqual('Tania Gonzales');
+    });
+  });
+
+  var getGroupName = function(index) {
+    return getGroup(index).findElement(by.binding('group')).getText();
+  };
+
+  var getGroupContacts = function(index) {
+    return getGroup(index).findElements(by.repeater('contact in contacts'));
+  };
+
+  var getGroup = function(index) {
+    return groups.get(index);
+  };
 });
